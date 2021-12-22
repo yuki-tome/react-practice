6ca8cf10de6119db6096cd2356969badf472195a
YukiTome <63280039+yuki-tome@users.noreply.github.com>
2021-12-22 22:08:00 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
